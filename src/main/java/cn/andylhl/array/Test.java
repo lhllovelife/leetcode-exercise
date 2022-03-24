326af1dc8d7434881ecdc0b0a1f2efcef1899651
lihongliang <lihongliang@xylink.com>
2022-03-25 00:20:22 +0800

    第一次正式提交

---
+++ src/main/java/cn/andylhl/array/Test.java
@@ -0,0 +1,15 @@
+package cn.andylhl.array;
+
+/**
+ * @author lhl
+ * @version 1.0.0
+ * @ClassName Test.java
+ * @Description TODO
+ * @createTime 2022年03月25日 00:18:00
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("first commit");
+    }
+}
